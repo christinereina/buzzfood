c18b77ab1c0d34fa17b4c91dd95282d80fc55a3b
blacchoy@gmail.com <blacchoy@gmail.com>
2020-06-05 14:43:09 -0700

    add branching for quiz results - ruby result successful

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,27 @@
 $(document).ready(function() {
     $("form#quiz").submit(function(event) {
       event.preventDefault();
+
       const question1 = parseInt($("input:radio[name=q1]:checked").val());
       const question2 = parseInt($("input:radio[name=q2]:checked").val());
-      console.log(question2);
       const question3 = parseInt($("input:radio[name=q3]:checked").val());
-      console.log(question3);
       const question4 = parseInt($("input:radio[name=q4]:checked").val());
-      console.log(question4);
       const question5 = parseInt($("input:radio[name=q5]:checked").val());
-      console.log(question5);
-      let result = (question1 + question2 + question3 +  question4 + question5);
-      console.log(result);
+      const result = (question1 + question2 + question3 +  question4 + question5);
+
+      if (result < 6 ) {
+        $("#ruby").slideToggle();
+        $(".quizForm").hide();
+      }
+
+      else if (result <= 7 && result <= 10) {
+
+      }
+      else if (result >= 10 && result <= 15) {
+
+      }
+    $('#retake').click(function(){
+      location.reload();
+    })
   });
 });
