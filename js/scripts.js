8b890f95d9f6baa44ba948fc44f5ebfb6c4adac7
blacchoy@gmail.com <blacchoy@gmail.com>
2020-06-05 12:56:19 -0700

    add beginning javascript declaring const variables

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,18 @@
 $(document).ready(function() {
-  $("#form").submit(function(event) {
+    $("form#quiz").submit(function(event) {
+      event.preventDefault();
+      const question1 = parseInt($("input:radio[name=q1]:checked").val());
+      console.log(question1);
+      const question2 = parseInt($("input:radio[name=q2]:checked").val());
+      console.log(question2;
+      const question3 = parseInt($("input:radio[name=q3]:checked").val());
+      console.log(question3);
+      const question4 = parseInt($("input:radio[name=q4]:checked").val());
+      console.log(question4);
+      const question5 = parseInt($("input:radio[name=q5]:checked").val());
+      console.log(question5);
+      const result = question1 + question2 + question3 +  question4 + question5;
+      console.log(result);
 
   });
 });
